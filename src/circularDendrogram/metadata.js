0c1635c85d56909587aea10920fd4cebdbbecf11
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:45:35 +0800

    Update metadata.js

--- src/circularDendrogram/metadata.js
+++ src/circularDendrogram/metadata.js
@@ -2,13 +2,13 @@ import icon from './circulardendrogram.svg'
 import thumbnail from './circulardendrogram_thumb.svg'
 
 export const metadata = {
-  name: 'Circular dendrogram',
+  name: '圆形树状图',
   id: 'rawgraphs.circulardendrogram',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays hierarchically structured data with a radial tree structure, where the root node is in the center with the hierarchies moving outward. The area of nodes can be used to encode a further quantitative dimension and a quantitative or categorical dimension with color.',
+    '它以放射状树结构显示分层结构的数据，其中根节点位于中心，层次结构向外移动。 节点的面积可用于编码进一步的定量维度和带有颜色的定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/circularDendrogram',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-circular-dendrogram/',
