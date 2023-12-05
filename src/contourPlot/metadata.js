94eeb19014e6f2eb8b4f0254dd121ddc40846934
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:46:59 +0800

    Update metadata.js

--- src/contourPlot/metadata.js
+++ src/contourPlot/metadata.js
@@ -2,13 +2,13 @@ import icon from './contourPlot.svg'
 import thumbnail from './contourPlot_thumb.svg'
 
 export const metadata = {
-  name: 'Contour plot',
+  name: '等值线图',
   id: 'rawgraphs.contourplot',
   thumbnail,
   icon,
   categories: ['correlations', 'distributions'],
   description:
-    'It shows the estimated density of point clouds, which is especially useful to avoid overplotting in large datasets.',
+    '它显示了点云的估计密度，这对于避免大型数据集中的过度绘制特别有用。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/contourPlot',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-contour-plot/',
