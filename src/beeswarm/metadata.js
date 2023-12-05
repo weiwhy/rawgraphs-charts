42f7a77e8e7080f42fae62d0ada7f31182daea70
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:31:39 +0800

    Update metadata.js

--- src/beeswarm/metadata.js
+++ src/beeswarm/metadata.js
@@ -2,13 +2,13 @@ import icon from './beeswarm.svg'
 import thumbnail from './beeswarm_thumb.svg'
 
 export const metadata = {
-  name: 'Beeswarm plot',
+  name: '蜂群图',
   id: 'rawgraphs.beeswarm',
   thumbnail,
   icon,
   categories: ['distributions', 'time series', 'proportions'],
   description:
-    'It displays the distribution of items over a continuous dimensions. Each (line) is represented with a dot placed on the horizontal axis. The vertical dimension is used to avoid overlaps among circles, showing their distribution. The area of dots can be used to encode a further quantitative dimension and a quantitative or categorical dimension with color.',
+    '它显示项目在连续维度上的分布。每条（线）都用放置在水平轴上的一个点表示。垂直尺寸用于避免圆圈之间的重叠，显示它们的分布。点的面积可用于编码进一步的定量维度和带有颜色的定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/beeswarm',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-beeswarm-plot/',
