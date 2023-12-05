b02a8f8851d6509871867e4c48a3b0178757615c
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:20:45 +0800

    Update metadata.js

--- src/arcdiagram/metadata.js
+++ src/arcdiagram/metadata.js
@@ -2,13 +2,13 @@ import icon from './arcdiagram.svg'
 import thumbnail from './arcdiagram_thumb.svg'
 
 export const metadata = {
-  name: 'Arc Diagram',
+  name: '弧线图',
   id: 'rawgraphs.arcdiagram',
   thumbnail,
   icon,
   categories: ['networks'],
   description:
-    'A particular kind of network graph, allows seeing relationships among nodes. Nodes are displayed on the horizontal axis, and links as clockwise arcs. An arc above the nodes means a connection from the left to the right, while below means a connection from the right node to the left one.',
+    '一种特殊的网络图，可查看节点之间的关系。节点显示在横轴上，链接显示为顺时针方向的弧线。节点上方的弧线表示从左边到右边的连接，而下方的弧线表示从右边节点到左边节点的连接.',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/arcdiagram',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-an-arc-diagram/',
