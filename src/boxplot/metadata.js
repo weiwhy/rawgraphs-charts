bec21fb611b96c8551adedfaadf32108c89b52ed
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:33:47 +0800

    Update metadata.js

--- src/boxplot/metadata.js
+++ src/boxplot/metadata.js
@@ -2,13 +2,13 @@ import icon from './boxplot.svg'
 import thumbnail from './boxplot_thumb.svg'
 
 export const metadata = {
-  name: 'Box plot',
+  name: '箱形图',
   id: 'rawgraphs.boxplot',
   thumbnail,
   icon,
   categories: ['distributions'],
   description:
-    'It summarize a quantitative distribution with five standard statistics: the smallest value, lower quartile, median, upper quartile, and largest value.',
+    '它总结了具有五个标准统计量的定量分布：最小值、下四分位数、中位数、上四分位数和最大值。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/boxplot',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-boxplot/',
 }
