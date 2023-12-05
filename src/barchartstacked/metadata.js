3510e5838859763e2d00bee139bac72224dd67bc
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:26:13 +0800

    Update metadata.js

--- src/barchartstacked/metadata.js
+++ src/barchartstacked/metadata.js
@@ -2,13 +2,13 @@ import icon from './barchartstacked.svg'
 import thumbnail from './barchartstacked_thumb.svg'
 
 export const metadata = {
-  name: 'Stacked bar chart',
+  name: '堆叠条形图',
   id: 'rawgraphs.barchartstacked',
   thumbnail,
   icon,
   categories: ['correlations', 'proportions'],
   description:
-    'It displays multiple quantitative dimensions related to categories. bars are visually stacked according to the categorical dimension, each bar represents a quantitative dimension, mapped on its height.',
+    '它显示与类别相关的多个定量维度。条形图根据分类维度直观地堆叠在一起，每个条形图代表一个定量维度，并根据其高度进行映射。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/barchartstacked',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-stacked-barchart/',
