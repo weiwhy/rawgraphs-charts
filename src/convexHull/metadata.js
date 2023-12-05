a82a9292579625a343768fb92c3dba82e494df62
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:50:14 +0800

    Update metadata.js

--- src/convexHull/metadata.js
+++ src/convexHull/metadata.js
@@ -2,13 +2,13 @@ import icon from './convexHull.svg'
 import thumbnail from './convexHull_thumb.svg'
 
 export const metadata = {
-  name: 'Convex hull',
+  name: '凸包图',
   id: 'rawgraphs.convexhull',
   thumbnail,
   icon,
   categories: ['correlations', 'proportions'],
   description:
-    'In mathematics, the convex hull is the smallest convex shape containing a set of points. Applied to a scatterplot, it is useful to identify points belonging to the same category.',
+    '在数学中，凸包是包含一组点的最小凸形状。 应用于散点图，识别属于同一类别的点很有用。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/convexHull',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-convex-hulls-chart/',
