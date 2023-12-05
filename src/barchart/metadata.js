c1c046c963e0cda62ecfab62970317e66534b66d
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:23:29 +0800

    Update metadata.js

--- src/barchart/metadata.js
+++ src/barchart/metadata.js
@@ -2,13 +2,13 @@ import icon from './barchart.svg'
 import thumbnail from './barchart_thumb.svg'
 
 export const metadata = {
-  name: 'Bar chart',
+  name: '条形图',
   thumbnail,
   icon,
   id: 'rawgraphs.barchart',
   categories: ['correlations'],
   description:
-    'It displays a categorical dimension and related amounts. Each bar represents a category, width is proportional to the quantitative dimension.',
+    '它显示分类维度和相关数量。每个条形图代表一个类别，宽度与数量维度成正比。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/barchart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-barchart/',
