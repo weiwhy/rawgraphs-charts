fd3a7e351c2dc7577cbe579382231ca8438b359e
weiwhy <why1573920133@hotmail.com>
2023-12-05 17:10:23 +0800

    Update metadata.js

--- src/alluvialdiagram/metadata.js
+++ src/alluvialdiagram/metadata.js
@@ -2,7 +2,7 @@ import icon from './alluvialdiagram.svg'
 import thumbnail from './alluvialdiagram_thumb.svg'
 
 export const metadata = {
-  name: 'Alluvial Diagram',
+  name: '冲积图',
   id: 'rawgraphs.alluvialdiagram',
   thumbnail,
   icon,
@@ -11,5 +11,5 @@ export const metadata = {
   tutorial: 'https://rawgraphs.io/learning/how-to-make-an-alluvial-diagram/',
   categories: ['correlations', 'proportions'],
   description:
-    'It shows correlations between categorical dimensions representing them as flows, visually linking categories with shared items. Each rectangle represents a unique value in the selected dimension, its height is proportional to its value. Correlations are represented with curved lines whose width is proportional to their value.',
+    '它显示了分类维度之间的相关性，将其表示为流动，直观地将类别与共享项目联系起来。每个矩形代表所选维度中的一个唯一值，其高度与其值成正比。相关性用曲线表示，其宽度与数值成正比。',
 }
