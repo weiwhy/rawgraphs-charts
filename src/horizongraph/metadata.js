c9bd6d0cc34533b50a903ce36b270128cd5673cf
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:56:28 +0800

    Update metadata.js

--- src/horizongraph/metadata.js
+++ src/horizongraph/metadata.js
@@ -2,13 +2,13 @@ import icon from './horizongraph.svg'
 import thumbnail from './horizongraph_thumb.svg'
 
 export const metadata = {
-  name: 'Horizon graph',
+  name: '地平线图',
   id: 'rawgraphs.horizongraph',
   thumbnail,
   icon,
   categories: ['time series', 'correlations'],
   description:
-    'It displays a quantitative dimension over a continuous interval or time period. Horizon graphs combine position and color to reduce vertical space.',
+    '它显示连续间隔或时间段内的定量维度。 水平图结合位置和颜色以减少垂直空间。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/horizongraph',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-horizon-graph/',
