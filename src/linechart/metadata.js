affce7628017c8ad801e72f71e67f865ce1102a9
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:57:32 +0800

    Update metadata.js

--- src/linechart/metadata.js
+++ src/linechart/metadata.js
@@ -2,13 +2,13 @@ import icon from './linechart.svg'
 import thumbnail from './linechart_thumb.svg'
 
 export const metadata = {
-  name: 'Line chart',
+  name: '折线图',
   id: 'rawgraphs.linechart',
   thumbnail,
   icon,
   categories: ['time series', 'correlations'],
   description:
-    'It displays a quantitative dimension over a continuous interval or time period. Colour can be optionally used to encode an additional quantitative or categorical dimension.',
+    '它显示连续间隔或时间段内的定量维度。 可以选择使用颜色来编码附加的定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/linechart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-line-chart/',
