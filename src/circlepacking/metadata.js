3dcba181f46692f4685e7ff515447fdb66b5e011
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:44:28 +0800

    Update metadata.js

--- src/circlepacking/metadata.js
+++ src/circlepacking/metadata.js
@@ -2,13 +2,13 @@ import icon from './circlepacking.svg'
 import thumbnail from './circlepacking_thumb.svg'
 
 export const metadata = {
-  name: 'Circle Packing',
+  name: '圆形包装图',
   id: 'rawgraphs.circlepacking',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays values of leaf nodes of a hierarchical structure by using circles areas. The hierarchical structure is depicted using nested circles. A further quantitative dimension with size and a quantitative or categorical dimension with color.',
+    '它使用圆形区域显示层次结构的叶节点的值。 使用嵌套圆圈来描述层次结构。 带有大小的进一步定量维度和带有颜色的定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/circlepacking',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-circle-packing',
