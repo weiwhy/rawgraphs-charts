ea72e20d349fe428f7915166a42b9fe7be976a70
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:34:46 +0800

    Update metadata.js

--- src/bubblechart/metadata.js
+++ src/bubblechart/metadata.js
@@ -2,13 +2,13 @@ import icon from './bubblechart.svg'
 import thumbnail from './bubblechart_thumb.svg'
 
 export const metadata = {
-  name: 'Bubble chart',
+  name: '气泡图',
   id: 'rawgraphs.bubblechart',
   thumbnail,
   icon,
   categories: ['correlations', 'proportions'],
   description:
-    'The basic layout is a scatter plot, which allows to see correlations among two continuous dimensions. A further quantitative dimension with size and a quantitative or categorical dimension with color.',
+    '基本布局是散点图，它允许查看两个连续维度之间的相关性。具有大小的进一步定量维度和带有颜色的定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/bubblechart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-scatterplot/',
