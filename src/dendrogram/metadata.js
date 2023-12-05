fafcd06ae5ee7d6fef3e9c764a063d4e1c140f6b
weiwhy <why1573920133@hotmail.com>
2023-12-05 23:51:12 +0800

    Update metadata.js

--- src/dendrogram/metadata.js
+++ src/dendrogram/metadata.js
@@ -2,13 +2,13 @@ import icon from './dendrogram.svg'
 import thumbnail from './dendrogram_thumb.svg'
 
 export const metadata = {
-  name: 'Linear dendrogram',
+  name: '线性树状图',
   id: 'rawgraphs.lineardendrogram',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays hierarchically structured data with a tree structure, where the root node is on the left and leaves are on the right. The size of nodes can be used to encode a further quantitative dimension with size and a quantitative or categorical dimension with color.',
+    '它以树形结构显示分层结构的数据，其中根节点在左侧，叶子在右侧。 节点的大小可用于用大小编码进一步的定量维度以及用颜色编码定量或分类维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/dendrogram',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-linear-dendrogram/',
