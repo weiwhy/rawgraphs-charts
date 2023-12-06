8a578e190cf5b37f4c596649ce2fb54ddb76e0e1
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:19:35 +0800

    Update metadata.js

--- src/sunburst/metadata.js
+++ src/sunburst/metadata.js
@@ -2,13 +2,13 @@ import icon from './sunburst.svg'
 import thumbnail from './sunburst_thumb.svg'
 
 export const metadata = {
-  name: 'Sunburst diagram',
+  name: '旭日图',
   id: 'rawgraphs.sunburst',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays hierarchically structured data and a related quantitative dimension using concentric circles. The circle in the center represents the root node, with the hierarchies moving outward from the center. The angle of each arc corresponds to the qualitative dimension.',
+    '它使用同心圆显示分层结构的数据和相关的定量维度。 中心的圆圈代表根节点，层次结构从中心向外移动。 每条弧线的角度对应于定性维度。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/sunburst',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-sunburst-diagram/',
