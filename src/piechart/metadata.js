d8fda6bdb7bf3ceb0a985ff1a468f83276a6a198
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:22:42 +0800

    Update metadata.js

--- src/piechart/metadata.js
+++ src/piechart/metadata.js
@@ -2,13 +2,13 @@ import icon from './piechart.svg'
 import thumbnail from './piechart_thumb.svg'
 
 export const metadata = {
-  name: 'Pie chart',
+  name: '饼形图',
   id: 'rawgraphs.piechart',
   thumbnail,
   icon,
   categories: ['proportions'],
   description:
-    'It allows you to see the proportions between values that make up a whole, by using arcs composing a circle.',
+    '它允许您通过使用构成圆的弧来查看构成整体的值之间的比例。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/piechart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-pie-chart/',
 }
