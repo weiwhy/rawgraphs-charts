067a272ed3ccfc35a3098e79efd62614194bc58e
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:22:15 +0800

    Update metadata.js

--- src/radarchart/metadata.js
+++ src/radarchart/metadata.js
@@ -2,13 +2,13 @@ import icon from './radarchart.svg'
 import thumbnail from './radarchart_thumb.svg'
 
 export const metadata = {
-  name: 'Radar Chart',
+  name: '雷达图',
   id: 'rawgraphs.radarchart',
   thumbnail,
   icon,
   categories: ['correlations'],
   description:
-    'It displays multiple continuous dimensions as axes starting from the same point and by disposing them radially. Each dimension is represented as an axis starting from the center of the cart. The same scale is applied to all the axes.',
+    '它将多个连续尺寸显示为从同一点开始并呈放射状排列的轴。 每个维度都表示为从购物车中心开始的轴。 相同的比例应用于所有轴。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/radarchart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-radar-chart/',
