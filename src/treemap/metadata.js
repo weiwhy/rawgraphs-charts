b789536d6b674a02386ce8f4918a971585e87e77
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:18:57 +0800

    Update metadata.js

--- src/treemap/metadata.js
+++ src/treemap/metadata.js
@@ -2,13 +2,13 @@ import icon from './treemap.svg'
 import thumbnail from './treemap_thumb.svg'
 
 export const metadata = {
-  name: 'Treemap',
+  name: '树形图',
   id: 'rawgraphs.treemap',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays hierarchically structured data and a related quantitative dimension. It is composed of an area divided into small rectangles, representing the last level of the tree structure. The rectangles’ size depends on the quantitative dimension.',
+    '它显示分层结构的数据和相关的定量维度。 它由划分为小矩形的区域组成，代表树结构的最后一层。 矩形的大小取决于数量维度。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/treemap',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-treemap/',
 }
