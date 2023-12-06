5b3c2b311666aa70506f811939db916fdcc51245
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:21:06 +0800

    Update metadata.js

--- src/slopechart/metadata.js
+++ src/slopechart/metadata.js
@@ -2,13 +2,13 @@ import icon from './slopechart.svg'
 import thumbnail from './slopechart_thumb.svg'
 
 export const metadata = {
-  name: 'Slope chart',
+  name: '坡度图',
   id: 'rawgraphs.slopechart',
   thumbnail,
   icon,
   categories: ['correlations'],
   description:
-    'It allows the comparison of two continuous dimensions showing them as axes, and using a line to show the relationship.',
+    '它允许比较两个连续的维度，将它们显示为轴，并使用一条线来显示关系。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/slopechart',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-slope-graph/',
 }
