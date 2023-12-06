0252099c1ef48bd3fe51c380b504d636529063e7
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:21:42 +0800

    Update metadata.js

--- src/sankeydiagram/metadata.js
+++ src/sankeydiagram/metadata.js
@@ -2,13 +2,13 @@ import icon from './sankeydiagram.svg'
 import thumbnail from './sankeydiagram_thumb.svg'
 
 export const metadata = {
-  name: 'Sankey Diagram',
+  name: '桑基图',
   id: 'rawgraphs.sankeydiagram',
   thumbnail,
   icon,
   categories: ['networks'],
   description:
-    'It represents flows among nodes of a network. Nodes are represented as rectangles, the height represents their value. Flows are represented with curved lines whose width is proportional to their value.',
+    '它表示网络节点之间的流量。 节点表示为矩形，高度表示它们的值。 流量用曲线表示，其宽度与其值成正比。',
   code:
     'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/sankeydiagram',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-sankey-diagram/',
