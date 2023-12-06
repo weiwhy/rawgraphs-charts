84dfe50fb127af47ccf075b06a8ca2953059b957
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:16:11 +0800

    Update metadata.js

--- src/voronoitreemap/metadata.js
+++ src/voronoitreemap/metadata.js
@@ -2,13 +2,13 @@ import icon from './voronoitreemap.svg'
 import thumbnail from './voronoitreemap_thumb.svg'
 
 export const metadata = {
-  name: 'Treemap (Voronoi)',
+  name: '树形图 (Voronoi)',
   id: 'rawgraphs.voronoitreemap',
   thumbnail,
   icon,
   categories: ['hierarchies', 'proportions'],
   description:
-    'It displays hierarchically structured data and a related quantitative dimension. It is composed of an area divided into small cells, representing the last level of the tree structure, computed using the Voronoi tessellation. The cells’ size depends on the quantitative dimension. the dimensions are calculated iteratively, therefore area could be not fully representative of the mapped value.',
+    '它显示分层结构的数据和相关的定量维度。 它由划分为小单元的区域组成，代表树结构的最后一层，使用 Voronoi 曲面细分计算。 细胞的大小取决于数量维度。 尺寸是迭代计算的，因此面积不能完全代表映射值。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/treemap',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-treemap/',
 }
