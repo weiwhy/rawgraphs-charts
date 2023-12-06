f0b6e8f1d005519c37e4e89bde245a0d3be87f09
weiwhy <why1573920133@hotmail.com>
2023-12-06 10:16:55 +0800

    Update metadata.js

--- src/voronoidiagram/metadata.js
+++ src/voronoidiagram/metadata.js
@@ -2,13 +2,13 @@ import icon from './voronoidiagram.svg'
 import thumbnail from './voronoidiagram_thumb.svg'
 
 export const metadata = {
-  name: 'Voronoi Diagram',
+  name: '沃罗诺伊图',
   id: 'rawgraphs.voronoidiagram',
   thumbnail,
   icon,
   categories: ['Correlations'],
   description:
-    'It creates the minimum area around each point defined by two variables. When applied to a scatterplot, it is useful to show the distance between points.',
+    '它创建由两个变量定义的每个点周围的最小区域。 当应用于散点图时，显示点之间的距离很有用。',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/voronoidiagram',
   tutorial: 'https://rawgraphs.io/learning/how-to-make-a-voronoi-diagram/',
 }
